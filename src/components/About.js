70e98c10299b22ba46fe4918ad02e30a0b8046b8
Selin Su Varol <selinsuvarol@gmail.com>
2022-06-06 14:08:52 +0300

    completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,12 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+  <div id="about">
+    <h2>About Me</h2>
+    <p>I am a software engineer who is passionate about building web applications. I have a background in computer science and a passion for learning new things. I am currently working as a software engineer at <a href="https://www.linkedin.com/company/linkedin/">LinkedIn</a>.</p>
+    <img src={image} alt="I made this" />
+  </div>);
 }
 
 export default About;
